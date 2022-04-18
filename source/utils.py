d8127a0f41e2d11fb94391d942711a58dab6023e
Yoonchang Sung <yooncs8@csail.mit.edu>
2022-04-18 13:39:04 -0500

    Add visualization methods in utils

--- source/utils.py
+++ source/utils.py
@@ -2,6 +2,9 @@ import numpy as np
 import math
 import random
 import torch
+import itertools
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 
 def read_off(file):
@@ -57,6 +60,7 @@ class PointSampler(object):
         
         return sampled_points
 
+
 class Normalize(object):
     def __call__(self, pointcloud):
         assert len(pointcloud.shape)==2
@@ -65,6 +69,7 @@ class Normalize(object):
         norm_pointcloud /= np.max(np.linalg.norm(norm_pointcloud, axis=1))
 
         return  norm_pointcloud
+
 
 class RandRotation_z(object):
     def __call__(self, pointcloud):
@@ -77,7 +82,8 @@ class RandRotation_z(object):
         
         rot_pointcloud = rot_matrix.dot(pointcloud.T).T
         return  rot_pointcloud
-    
+
+
 class RandomNoise(object):
     def __call__(self, pointcloud):
         assert len(pointcloud.shape)==2
@@ -86,9 +92,84 @@ class RandomNoise(object):
     
         noisy_pointcloud = pointcloud + noise
         return  noisy_pointcloud
-        
+
+
 class ToTensor(object):
     def __call__(self, pointcloud):
         assert len(pointcloud.shape)==2
 
         return torch.from_numpy(pointcloud)
+
+
+def visualize_rotate(data):
+    x_eye, y_eye, z_eye = 1.25, 1.25, 0.8
+    frames=[]
+
+    def rotate_z(x, y, z, theta):
+        w = x+1j*y
+        return np.real(np.exp(1j*theta)*w), np.imag(np.exp(1j*theta)*w), z
+
+    for t in np.arange(0, 10.26, 0.1):
+        xe, ye, ze = rotate_z(x_eye, y_eye, z_eye, -t)
+        frames.append(dict(layout=dict(scene=dict(camera=dict(eye=dict(x=xe, y=ye, z=ze))))))
+    fig = go.Figure(data=data,
+                    layout=go.Layout(
+                        updatemenus=[dict(type='buttons',
+                                    showactive=False,
+                                    y=1,
+                                    x=0.8,
+                                    xanchor='left',
+                                    yanchor='bottom',
+                                    pad=dict(t=45, r=10),
+                                    buttons=[dict(label='Play',
+                                                    method='animate',
+                                                    args=[None, dict(frame=dict(duration=50, redraw=True),
+                                                                    transition=dict(duration=0),
+                                                                    fromcurrent=True,
+                                                                    mode='immediate'
+                                                                    )]
+                                                    )
+                                            ]
+                                    )
+                                ]
+                    ),
+                    frames=frames
+            )
+
+    return fig
+
+
+def pcshow(xs,ys,zs):
+    data=[go.Scatter3d(x=xs, y=ys, z=zs,
+                                   mode='markers')]
+    fig = visualize_rotate(data)
+    fig.update_traces(marker=dict(size=2,
+                      line=dict(width=2,
+                      color='DarkSlateGrey')),
+                      selector=dict(mode='markers'))
+    fig.show()
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
