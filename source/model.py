9f5ce7ca0562d6e5e361fafad360cee1983d34c7
Yoonchang Sung <yooncs8@csail.mit.edu>
2022-04-18 13:39:34 -0500

    Ad hoc change to model to turn off batchnorm. Turn it on later

--- source/model.py
+++ source/model.py
@@ -14,23 +14,28 @@ class Tnet(nn.Module):
       self.fc2 = nn.Linear(512,256)
       self.fc3 = nn.Linear(256,k*k)
 
-      self.bn1 = nn.BatchNorm1d(64)
-      self.bn2 = nn.BatchNorm1d(128)
-      self.bn3 = nn.BatchNorm1d(1024)
-      self.bn4 = nn.BatchNorm1d(512)
-      self.bn5 = nn.BatchNorm1d(256)
+      # self.bn1 = nn.BatchNorm1d(64)
+      # self.bn2 = nn.BatchNorm1d(128)
+      # self.bn3 = nn.BatchNorm1d(1024)
+      # self.bn4 = nn.BatchNorm1d(512)
+      # self.bn5 = nn.BatchNorm1d(256)
        
 
    def forward(self, input):
       # input.shape == (bs,n,3)
       bs = input.size(0)
-      xb = F.relu(self.bn1(self.conv1(input)))
-      xb = F.relu(self.bn2(self.conv2(xb)))
-      xb = F.relu(self.bn3(self.conv3(xb)))
+      # xb = F.relu(self.bn1(self.conv1(input)))
+      # xb = F.relu(self.bn2(self.conv2(xb)))
+      # xb = F.relu(self.bn3(self.conv3(xb)))
+      xb = F.relu(self.conv1(input))
+      xb = F.relu(self.conv2(xb))
+      xb = F.relu(self.conv3(xb))
       pool = nn.MaxPool1d(xb.size(-1))(xb)
       flat = nn.Flatten(1)(pool)
-      xb = F.relu(self.bn4(self.fc1(flat)))
-      xb = F.relu(self.bn5(self.fc2(xb)))
+      # xb = F.relu(self.bn4(self.fc1(flat)))
+      # xb = F.relu(self.bn5(self.fc2(xb)))
+      xb = F.relu(self.fc1(flat))
+      xb = F.relu(self.fc2(xb))
       
       #initialize as identity
       init = torch.eye(self.k, requires_grad=True).repeat(bs,1,1)
@@ -51,22 +56,25 @@ class Transform(nn.Module):
         self.conv3 = nn.Conv1d(128,1024,1)
        
 
-        self.bn1 = nn.BatchNorm1d(64)
-        self.bn2 = nn.BatchNorm1d(128)
-        self.bn3 = nn.BatchNorm1d(1024)
+        # self.bn1 = nn.BatchNorm1d(64)
+        # self.bn2 = nn.BatchNorm1d(128)
+        # self.bn3 = nn.BatchNorm1d(1024)
        
    def forward(self, input):
         matrix3x3 = self.input_transform(input)
         # batch matrix multiplication
         xb = torch.bmm(torch.transpose(input,1,2), matrix3x3).transpose(1,2)
 
-        xb = F.relu(self.bn1(self.conv1(xb)))
+        # xb = F.relu(self.bn1(self.conv1(xb)))
+        xb = F.relu(self.conv1(xb))
 
         matrix64x64 = self.feature_transform(xb)
         xb = torch.bmm(torch.transpose(xb,1,2), matrix64x64).transpose(1,2)
 
-        xb = F.relu(self.bn2(self.conv2(xb)))
-        xb = self.bn3(self.conv3(xb))
+        # xb = F.relu(self.bn2(self.conv2(xb)))
+        # xb = self.bn3(self.conv3(xb))
+        xb = F.relu(self.conv2(xb))
+        xb = self.conv3(xb)
         xb = nn.MaxPool1d(xb.size(-1))(xb)
         output = nn.Flatten(1)(xb)
         return output, matrix3x3, matrix64x64
@@ -80,15 +88,17 @@ class PointNet(nn.Module):
         self.fc3 = nn.Linear(256, classes)
         
 
-        self.bn1 = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(256)
+        # self.bn1 = nn.BatchNorm1d(512)
+        # self.bn2 = nn.BatchNorm1d(256)
         self.dropout = nn.Dropout(p=0.3)
         self.logsoftmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input):
         xb, matrix3x3, matrix64x64 = self.transform(input)
-        xb = F.relu(self.bn1(self.fc1(xb)))
-        xb = F.relu(self.bn2(self.dropout(self.fc2(xb))))
+        # xb = F.relu(self.bn1(self.fc1(xb)))
+        # xb = F.relu(self.bn2(self.dropout(self.fc2(xb))))
+        xb = F.relu(self.fc1(xb))
+        xb = F.relu(self.dropout(self.fc2(xb)))
         output = self.fc3(xb)
         return self.logsoftmax(output), matrix3x3, matrix64x64
 
